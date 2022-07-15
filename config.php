6038f4c74d0c699c1a7841acdfe8bfa6748cd42c
slbarriosdev <85902820+slbarriosdev@users.noreply.github.com>
2022-07-15 11:20:08 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+    define("URL","");
+    define("SENDGRID_API_KEY","");
+    define("FILEDIR","C:\xampp\htdocs\ps-webscan");
+    define("FILENAME","log.txt");
+	  define("FROM","");
+	  define("TO","");
+	  define("CC","");        
+
+?>
